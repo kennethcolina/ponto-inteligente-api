a6dd9c210656fdc182e709f6824293c82d5b39b3
Kenneth Colina <kennethcolina@gmail.com>
2018-04-04 05:20:33 -0300

    criar entidades

---
+++ src/main/java/entities/Empresa.java
@@ -0,0 +1,15 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+    @Id
+
+
+}
